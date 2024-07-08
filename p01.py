20425cbc17703ef668fca6d2eb0b73f1220e6d76
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-07-08 20:13:19 +0530

    Add files via upload

---
+++ p01.py
@@ -0,0 +1,5 @@
+#Write a program to print the sum of two numbers
+a = 2
+b = 5
+sum = a + b
+print(sum)
---
+++ p02.py
@@ -0,0 +1,5 @@
+#Taking input from user & printing it
+name = input("Name: ")
+age = int(input("Age: "))
+
+print("My name is", name, "and I am", age, "years old")
