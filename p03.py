39ac113402ce073f1ab221923ce5564be25e3632
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-07-09 10:48:02 +0530

    Add files via upload

---
+++ p03.py
@@ -0,0 +1,11 @@
+#Traffic Lights Code
+
+light = input("light color: ")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("light is broken")
---
+++ p05.py
@@ -0,0 +1,5 @@
+#Single Line if/ Ternary Operator
+
+food = input("food : ")
+eat = "yes" if food == "cake" else "no"
+print(eat)
