8c230c5d37a573ba106cc6737fdba16019a68b3e
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-07-11 19:10:13 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,6 @@
+#WAP to input user's first name & print its length
+
+name = input("Enter your first name : ")
+
+l = len(name)
+print("Length : ", l)
---
+++ p11.py
@@ -0,0 +1,5 @@
+#WAP to find the occurrence of '$'in a string
+
+str = input("Enter a string : ")
+c = str.count("$")
+print("Occurrence : ", c)
