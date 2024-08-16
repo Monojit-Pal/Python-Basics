3f0a371031f86356a35822c83888f2e6a80a00f9
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-08-16 16:22:51 +0530

    Add files via upload

---
+++ p53.py
@@ -0,0 +1,5 @@
+complex_number = complex(input("Enter a complex number (e.g., 3+4j): "))
+real_part = complex_number.real
+imaginary_part = complex_number.imag
+print("Real part =", real_part)
+print("Complex part =", imaginary_part)
