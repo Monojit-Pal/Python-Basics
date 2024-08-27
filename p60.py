ba2a6bec3c54f096bb50ff1534e6b8f2f989547e
Monojit-Pal <144504389+Monojit-Pal@users.noreply.github.com>
2024-08-27 21:15:35 +0530

    Add files via upload

---
+++ p60.py
@@ -0,0 +1,7 @@
+#Write a program which takes input of a number and check whether it is even or odd.
+
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
